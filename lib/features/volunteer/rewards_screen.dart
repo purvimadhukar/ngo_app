08ed5f545439b0429e45a4f452612bdb762464e1
purvimadhukar <purvim071@gmail.com>
2026-04-16 18:47:06 +0530

    chore: update

--- lib/features/volunteer/rewards_screen.dart
+++ lib/features/volunteer/rewards_screen.dart
@@ -139,7 +139,7 @@ class _ProgressBar extends StatelessWidget {
         .toList()
       ..sort((a, b) => a.pointsRequired.compareTo(b.pointsRequired));
 
-    RewardRewardRewardBadge? nextBadge;
+    RewardBadge? nextBadge;
     int prevPoints = 0;
     for (final badge in sorted) {
       if (points < badge.pointsRequired) {
